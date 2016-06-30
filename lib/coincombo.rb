443a28a4a06671f339b763d179731b8fdd9ca7c5
Brian Grant and Cameron Poore <cameron@>
2016-06-30 09:35:49 -0700

    refactor

--- lib/coincombo.rb
+++ lib/coincombo.rb
@@ -3,29 +3,17 @@ require("pry")
 class String
   define_method(:coincombo) do
     change = self.to_i()
-    # coins = {"25" => "Quarter", "10" => "Dime", "5" => "Nickel", "1" => "Penny"}
-    # if coins.include?(self)
-    #   coins.fetch(self)
-    # end
-    quarterCounter = 0
-    dimeCounter = 0
-    nickelCounter = 0
-    pennyCounter = 0
     resultArray = []
     quarterArray = change.divmod(25)
-    quarterCounter+=quarterArray.at(0)
-    resultArray.push(quarterCounter)
+    resultArray.push(quarterArray.at(0))
     resultArray.push("Quarters")
     dimeArray = quarterArray.at(1).divmod(10)
-    dimeCounter+=dimeArray.at(0)
-    resultArray.push(dimeCounter)
+    resultArray.push(dimeArray.at(0))
     resultArray.push("Dimes")
     nickelArray = dimeArray.at(1).divmod(5)
-    nickelCounter+=nickelArray.at(0)
-    resultArray.push(nickelCounter)
+    resultArray.push(nickelArray.at(0))
     resultArray.push("Nickels")
-    pennyCounter = nickelArray.at(1)
-    resultArray.push(pennyCounter)
+    resultArray.push(nickelArray.at(1))
     resultArray.push("Pennies")
     resultArray.join(" ")
   end
